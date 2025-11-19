f2eb64d39457739ff65aef82332729345da1a979
Kerim Imamović <imamovic.kerim@gmail.com>
2025-11-19 15:40:32 +0100

    Edit my hotel test

--- e2e/tests/manageHotel.spec.ts
+++ e2e/tests/manageHotel.spec.ts
@@ -69,3 +69,22 @@ test('should display hotels', async ({ page }) => {
   await expect(hotelCard.getByRole('link', { name: 'View Details' })).toBeVisible();
   await expect(page.getByRole('link', { name: 'Add Hotel' })).toBeVisible();
 });
+
+test("should edit hotel", async ({ page }) => {
+  await page.goto(`${UI_URL}/my-hotels`);
+  await page.getByRole("link", { name: "View Details" }).first().click();
+  await page.waitForSelector('[name="name"]', { state: "attached" });
+
+  await expect(page.locator('[name="name"]')).toHaveValue("My small test hotel");
+
+  await page.locator('[name="name"]').fill("My small test hotel UPDATED");
+  await page.getByRole("button", { name: "Save" }).click();
+
+  await expect(page.getByText("Hotel Saved!")).toBeVisible();
+
+  await page.reload();
+  await expect(page.locator('[name="name"]')).toHaveValue("My small test hotel UPDATED");
+
+  await page.locator('[name="name"]').fill("My small test hotel");
+  await page.getByRole("button", { name: "Save" }).click();
+});
